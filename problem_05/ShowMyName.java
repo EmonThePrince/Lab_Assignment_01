50cf8fd77bec65312deacc6f9e982b76340158e6
EmonThePrince <saimahmedemon7@gmail.com>
2023-07-17 19:34:28 +0600

    My Assignment

---
+++ problem_05/ShowMyName.java
@@ -0,0 +1,17 @@
+package problem_05;
+
+public class ShowMyName {
+    public static void main(String[] args) {
+        String name = "Emon";
+        int length = name.length() + 4;
+        String horizontalLine = "+" + "-".repeat(length) + "+";
+        String emptyLine = "|" + " ".repeat(length) + "|";
+
+        System.out.println(horizontalLine);
+        System.out.println(emptyLine);
+        System.out.println("|  " + name + "  |");
+        System.out.println(emptyLine);
+        System.out.println(horizontalLine);
+    }
+    
+}
